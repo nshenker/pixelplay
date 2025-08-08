7be4ea8d0878b9c1f71bd9d5d416ba49ae0c5250
nshenker <64337739+nshenker@users.noreply.github.com>
2025-08-08 11:07:10 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type AppProps from "next/app";
 <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
 
 export default function App({ Component, pageProps }: AppProps) {
